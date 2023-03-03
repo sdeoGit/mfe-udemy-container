fa305fd445c8fe534ad6db48b40e4869727dd825
deosidd <Siddarth.deo@carrier.com>
2023-03-03 12:54:07 +0530

    Added mount function

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,4 +1,7 @@
-import 'products/ProductsIndex';
-import 'cart/CartShow';
+import { mount as productsMount} from "products/ProductsIndex";
+import { mount as cartMount} from "cart/CartShow";
 
-console.log('Container!');
+console.log("Container!");
+
+productsMount(document.querySelector("#container-dev-products"));
+cartMount(document.querySelector("#container-dev-cart"));
